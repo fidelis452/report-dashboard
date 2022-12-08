a486c4da7ba310129bd8cb3d0a816d8a7895bba7
FIDELIS WAWERU <fideliswaweru19@gmail.com>
2022-12-08 17:10:34 +0300

    ui fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Index.jsx";
-import TableContent from "./components/content-table/TableContent.jsx";
-import Introduction from "./components/Intro/Index.jsx";
+// import TableContent from "./components/content-table/TableContent.jsx";
+// import Introduction from "./components/Intro/Index.jsx";
 import jsPDF from "jspdf";
-// import html2canvas from "html2canvas";
-// import * as htmlToImage from "html-to-image";
-// import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
+import "jspdf-autotable";
 import { useRef } from "react";
 import "./App.css";
-import { dblClick } from "@testing-library/user-event/dist/click.js";
 function App() {
   const Index = useRef(null);
 
@@ -23,9 +20,8 @@ function App() {
     });
 
     doc.setFont("Calibri", "normal");
-    // doc.text("*", )
-    // doc.text("my name is",10, 10,{lineHeightFactor: 1.5})
     const width = doc.internal.pageSize.getWidth();
+    doc.autotable({startY: 30})
     doc.html(Index.current, {
       autoPaging: "text",
       width: width,
@@ -37,17 +33,13 @@ function App() {
         doc.save("document");
       },
     });
-    // doc.splitTextToSize(Index.current, {
-    //   size: 10
-    // })
   };
 
   return (
     <div className="flex-container">
-      <div ref={Index}>
+      <div ref={Index} id="table">
         <Dashboard />
-        <TableContent/>
-        {/* <Introduction /> */}
+        {/* <TableContent /> */}
       </div>
       <div className="btn-classs">
         <button className="button" onClick={handleGeneratePdf}>
